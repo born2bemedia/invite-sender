ca14ce40974eafecfb3db98aa4bd0f9e12c5dd63
SergWebAcceptance <serg.web@acceptance.ltd>
2025-09-26 11:08:54 +0300

    added new email option for sender

--- src/app/senderOptions.js
+++ src/app/senderOptions.js
@@ -61,4 +61,5 @@ export const senderOptions = [
   { value: "socials@state-aware.com", label: "socials@state-aware.com" },
   { value: "socials@moduplans.com", label: "socials@moduplans.com" },
   { value: "socials@marketifire.com", label: "socials@marketifire.com" },
+  { value: "socials@marketeloro.com", label: "socials@marketeloro.com" },
 ];
